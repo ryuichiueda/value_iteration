4e2eeab4ba6bd3a692a58e667dd53e8b14016adf
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-01-08 14:23:55 +0900

    Make state cells 3D

--- src/ValueIterator.cpp
+++ src/ValueIterator.cpp
@@ -17,13 +17,12 @@ ValueIterator::ValueIterator(nav_msgs::OccupancyGrid &map)
 
 	_cell_x_width = map.info.resolution;
 	_cell_y_width = map.info.resolution;
-	_cell_t_width = 3.141592/72;
+	_cell_t_width = 360/_cell_t_num;
 
-	for(int y=0;y<_cell_y_num;y++){
-		for(int x=0;x<_cell_x_num;x++){
-			_states.push_back(State(x, y, 0, map.data[y*_cell_x_num + x]));
-		}
-	}
+	for(int y=0; y<_cell_y_num; y++)
+		for(int x=0; x<_cell_x_num; x++)
+			for(int t=0; t<_cell_t_num; t++)
+				_states.push_back(State(x, y, t, map.data[y*_cell_x_num + x]));
 }
 
 /* statesのセルの情報をPBMとして出力（デバッグ用） */
@@ -32,8 +31,11 @@ void ValueIterator::outputPbmMap(void){
 
 	ofs << "P1" << endl;
 	ofs << _cell_x_num << " " << _cell_y_num << endl;
-	for(auto s : _states)
-		ofs << s._free << " ";
+	int i = 0;
+	while(i<_states.size()){
+		ofs << _states[i]._free << " ";
+		i += _cell_t_num;
+	}
 
 	ofs << flush;
 }
