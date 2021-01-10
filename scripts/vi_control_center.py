4c3d003846a281c7d2c2f0e579c2ae0c3b17e709
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-01-10 13:35:17 +0900

    Implement feedback

--- scripts/vi_control_center.py
+++ scripts/vi_control_center.py
@@ -4,6 +4,9 @@ import rospy
 from value_iteration.msg import ViAction, ViGoal
 import actionlib
 
+def vi_state_cb(feedback):
+    print(feedback)
+
 def vi_client():
     print("enter")
     client = actionlib.SimpleActionClient('/vi_controller', ViAction)
@@ -12,7 +15,7 @@ def vi_client():
     print("ok")
 
     goal = ViGoal()
-    client.send_goal(goal)
+    client.send_goal(goal, feedback_cb=vi_state_cb)
     client.wait_for_result()
     return client.get_result()
 
@@ -20,3 +23,9 @@ if __name__ == '__main__':
     rospy.init_node('vi_controller')
     result = vi_client()
     print(result)
+
+    rate = rospy.Rate(1);
+    while not rospy.is_shutdown():
+        rate.sleep()
+
+    rospy.spin()
--- src/vi_node.cpp
+++ src/vi_node.cpp
@@ -12,12 +12,12 @@ using namespace std;
 class ViAction{
 
 public:
-	value_iteration::ViFeedback vi_feedback;
-	value_iteration::ViResult vi_result;
+	ValueIterator& _vi;
 	
 	actionlib::SimpleActionServer<value_iteration::ViAction> as;
 
-	ViAction(ros::NodeHandle &h, ValueIterator &vi) : as(h, "vi_controller", boost::bind(&ViAction::executeVi, this, _1), false)
+	ViAction(ros::NodeHandle &h, ValueIterator &vi) : as(h, "vi_controller", boost::bind(&ViAction::executeVi, this, _1), false),
+		_vi(vi)
 	{
 		as.start();
 		ROS_INFO("ViAction started");
@@ -25,6 +25,15 @@ public:
 	
 	void executeVi(const value_iteration::ViGoalConstPtr &goal)
 	{
+		for(int i=0;i<=10;i++){
+			sleep(1);
+			ROS_INFO("send feedback");
+			value_iteration::ViFeedback vi_feedback;
+			vi_feedback.current_sweep_time = i;
+			as.publishFeedback(vi_feedback);
+		}
+
+		value_iteration::ViResult vi_result;
 		vi_result.finished = false;
 		as.setSucceeded(vi_result);
 	}
