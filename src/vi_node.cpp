3c848b67f17359a332af97e06eeb77103bf4f55e
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-01-10 13:45:18 +0900

    Separate ViServe class from the main file

--- src/vi_node.cpp
+++ src/vi_node.cpp
@@ -5,39 +5,10 @@
 #include "nav_msgs/GetMap.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "ValueIterator.h"
+#include "ViServer.h"
 #include <iostream>
 #include <vector>
 using namespace std;
-
-class ViAction{
-
-public:
-	ValueIterator& _vi;
-	
-	actionlib::SimpleActionServer<value_iteration::ViAction> as;
-
-	ViAction(ros::NodeHandle &h, ValueIterator &vi) : as(h, "vi_controller", boost::bind(&ViAction::executeVi, this, _1), false),
-		_vi(vi)
-	{
-		as.start();
-		ROS_INFO("ViAction started");
-	}
-	
-	void executeVi(const value_iteration::ViGoalConstPtr &goal)
-	{
-		for(int i=0;i<=10;i++){
-			sleep(1);
-			ROS_INFO("send feedback");
-			value_iteration::ViFeedback vi_feedback;
-			vi_feedback.current_sweep_time = i;
-			as.publishFeedback(vi_feedback);
-		}
-
-		value_iteration::ViResult vi_result;
-		vi_result.finished = false;
-		as.setSucceeded(vi_result);
-	}
-};
 
 int main(int argc, char **argv)
 {
@@ -58,7 +29,7 @@ int main(int argc, char **argv)
 	}
 
 	ValueIterator value_iterator(res.map);
-	ViAction vi_action(n, value_iterator);
+	ViServer vi_server(n, value_iterator);
 //	value_iterator.outputPbmMap();
 
 	ros::spin();
