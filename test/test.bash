12cea7b2e0dca7235e66bb7d4cd178bea1fe6804
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-02-28 14:26:39 +0900

    Add policy comparison test

--- test/test.bash
+++ test/test.bash
@@ -4,3 +4,4 @@ source ~/.bashrc
 source ~/catkin_ws/devel/setup.bash
 roslaunch value_iteration test.launch || echo Finish
 [ "$(md5sum < ~/catkin_ws/src/value_iteration/test/value_t=3.pgm)" = "$(md5sum < /tmp/value_t=3.pgm)" ]
+[ "$(md5sum < ~/catkin_ws/src/value_iteration/test/action_t=41.ppm)" = "$(md5sum < /tmp/action_t=41.ppm)" ]
