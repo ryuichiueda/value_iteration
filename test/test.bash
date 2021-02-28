0743d280b8e431208f1871100d7b1bdb998859ca
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-02-28 14:21:54 +0900

    Fix the images

--- test/test.bash
+++ test/test.bash
@@ -1,5 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash -evx
 
 source ~/.bashrc
 source ~/catkin_ws/devel/setup.bash
-
+roslaunch value_iteration test.launch || echo Finish
+[ "$(md5sum < ~/catkin_ws/src/value_iteration/test/value_t=3.pgm)" = "$(md5sum < /tmp/value_t=3.pgm)" ]
