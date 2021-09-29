b51eb47349b1b20334cbeeded5a8f7be5433d95a
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-09-29 10:59:41 +0900

    Implement cancel procedure

--- scripts/vi_controller.py
+++ scripts/vi_controller.py
@@ -1,32 +1,53 @@
 #! /usr/bin/env python3
 
-import rospy
+import rospy, copy
 from value_iteration.msg import ViAction, ViGoal
 from geometry_msgs.msg import PoseStamped
 import actionlib
+
+goal_data = None
+client = None
 
 def vi_state_cb(feedback):
     rospy.loginfo("SWEEPS: " + ", ".join(str(e) for e in feedback.current_sweep_times.data))
     rospy.loginfo("DELTAS: " + ", ".join(str(e) for e in feedback.deltas.data))
 
 def vi_client(data):
+    global client 
+
     client = actionlib.SimpleActionClient('/vi_controller', ViAction)
-    old_client = client
     client.wait_for_server()
 
     goal = ViGoal()
-    goal.goal = data
+    goal.goal = copy.copy(data)
+    data = None
     client.send_goal(goal, feedback_cb=vi_state_cb)
     client.wait_for_result()
     return client.get_result()
 
 def receive_goal(data):
-    result = vi_client(data)
-    rospy.loginfo(result)
+    global goal_data
+    global client
+
+    if goal_data != None:
+        client.cancel_goal()
+
+    goal_data = data
 
 if __name__ == '__main__':
     rospy.init_node('vi_controller_turtle_env')
 
     sub_goal = rospy.Subscriber("/move_base_simple/goal", PoseStamped, receive_goal)
+
+    rate = rospy.Rate(20) 
+    while not rospy.is_shutdown():
+        if goal_data == None:
+            rate.sleep()
+            continue
+
+        result = vi_client(goal_data)
+        rospy.loginfo(result)
+
+        rate.sleep()
 
     rospy.spin()
